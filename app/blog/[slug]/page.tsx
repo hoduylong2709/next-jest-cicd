1660531f05ddb2b1236e30fc610fe68b833ef6d5
Spartan - Long Ho <long.ho@c0x12c.com>
2025-04-19 15:53:39 +0700

    fix deployment

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,6 +2,16 @@ type PageProps = {
   params: Promise<{ slug: string }>;
 };
 
+export async function generateStaticParams() {
+    // Define all the possible slug values for static generation
+    return [
+        { slug: 'first-post' },
+        { slug: 'second-post' },
+        { slug: 'Test' },
+        // Add all other slugs you need
+    ];
+}
+
 export async function generateMetadata({ params }: PageProps) {
   const { slug } = await params;
   return { title: `Post: ${slug}` };
