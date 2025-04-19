23e7222956b0d69c079c36341964f7b120a5fd8d
Spartan - Long Ho <long.ho@c0x12c.com>
2025-04-19 14:12:35 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,9 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug}<p>longho</p></h1>;
+  return <>
+    <h1>Slug: {params.slug}</h1>;
+    <p>longho</p>
+    <p>Welcome to Hue</p>
+  </>
 }
